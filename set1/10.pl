93feb86b24477c23a4b522e43f9306d082316f3b
EMerckx <ewoutmerckx.pro@gmail.com>
2015-11-10 15:14:22 +0100

    edited exercise 10

--- set1/10.pl
+++ set1/10.pl
@@ -1,18 +1,47 @@
-# De Load methode kan ook een OLE object als argument hebben. In Perl(Script) beschik je 
-# dus heel eenvoudig over alle constanten van een typelibrary die hoort bij een specifiek 
-# COM-object, ook al ken je de juiste naam niet.
-# Gebruik deze methode om een overzicht te tonen van alle constanten van de drie COM-object 
-# die we tot nu toe gezien hebben. Orden nu ook dit overzicht op de constantennaam.
-use Win32::OLE::Const;
+# Oefening 10
 
-@ARGV=("Excel.Sheet","Scripting.FileSystemObject","CDO.Message"); #lukt altijd.
+# De Load methode kan ook een OLE object als argument hebben.
+# In Perl(Script) beschik je dus heel eenvoudig over alle constanten
+# van een typelibrary die hoort bij een specifiek COM-object,
+# ook al ken je de juiste naam niet.
+# Gebruik deze methode om een overzicht te tonen van alle constanten
+# van de drie COM-object die we tot nu toe gezien hebben.
+# Orden nu ook dit overzicht op de constantennaam.
 
-for $comObjectNaam (@ARGV) {
-  print  "\n**********",$comObjectNaam,"***********\n";
-  $object = Win32::OLE->new($comObjectNaam);
-  %wd = %{Win32::OLE::Const->Load($object)}; #direct in een hash stoppen
+use strict;
+use warnings;
+use Win32::OLE::Const;
 
-  foreach (sort {$a cmp $b} keys %wd) {
-    printf ("%30s : %s\n",$_,$wd{$_});
-  }  
+# script stops and gives error message is something goes wrong
+$Win32::OLE::Warn = 3;
+
+# set the COM-objects
+# we have seen:
+# - "Excel.Sheet"
+# - "Scripting.FileSystemObject"
+# - "CDO.Message"
+@ARGV = ("Excel.Sheet");
+
+for my $comObjectName (@ARGV) {
+
+    # show the COM-object name
+    print $comObjectName . "\n\n";
+
+    # create the COM-object
+    my $object = Win32::OLE->new($comObjectName);
+
+    # load the constants of the COM-object
+    # and put it in a hash
+    my %constants = %{ Win32::OLE::Const->Load($object) };
+
+    # sort the hash and loop over each key
+    foreach ( sort { $a cmp $b } keys %constants ) {
+
+        printf( "%40s - %s \n", $_, $constants{$_} );
+
+        # to search for a specific key, eg xlEdgeRight, use:
+        # if (/xlEdgeRight/) {
+        #       printf( "%40s - %s \n", $_, $constants{$_} );
+        # }
+    }
 }
