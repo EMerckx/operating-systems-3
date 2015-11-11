2f76a15edec9a5381af75c66b8c81b8d58220f90
EMerckx <ewoutmerckx.pro@gmail.com>
2015-11-11 21:36:03 +0100

    exercise 17 added

---
+++ set5/17.ps1
@@ -0,0 +1,9 @@
+﻿# Oefening 17
+
+# Bepaal de naam van alle WMI-methodes van de klasse Win32_LogicalDisk
+
+clear
+
+$object = Get-WmiObject -List Win32_LogicalDisk
+$methods = $object.Methods
+$methods | select Name
