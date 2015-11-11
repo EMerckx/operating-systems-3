c83527a30701a26e21dec8390708348bfc853a8e
EMerckx <ewoutmerckx.pro@gmail.com>
2015-11-11 21:03:01 +0100

    exercise 16 added

---
+++ set5/16.ps1
@@ -0,0 +1,23 @@
+﻿# Oefening 16
+
+# Bepaal alle event-klassen die expliciet door een provider zijn 
+# toegevoegd (van welke klassen zijn ze afgeleid?).
+# Orden dit overzicht op aantal eigen attributen properties 
+# van elke klasse. 
+# Geef eventueel ook een lijst met de eigen properties 
+# voor elke klasse
+
+clear
+
+# get all the WMI objects as a list
+$list = Get-WmiObject -List
+
+# get all the explicit added classes
+$addedClasses = $list | 
+    where { $_.__DERIVATION -eq "__ExtrinsicEvent" } | 
+    select $_
+
+# show the class and property count of the explicit added classes
+$addedClasses | 
+    sort __Property_Count -Descending | 
+    select __Class, __Property_Count
