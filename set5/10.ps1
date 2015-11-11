03074345f90e5af993202a3efb3d727e018c15f1
EMerckx <ewoutmerckx.pro@gmail.com>
2015-11-11 22:29:39 +0100

    exercise 10 added

---
+++ set5/10.ps1
@@ -0,0 +1,21 @@
+﻿# Oefening 10
+
+# Bepaal het aantal instanties van Win32_LogicalDisk.
+# Toon enkel het relatief pad van elke instantie. 
+# Bekijk de mogelijkheden van het attribuut Path_
+
+clear
+
+# get the object
+$object = Get-WmiObject -Class Win32_LogicalDisk
+
+# count the instances
+$amount = $object.Count
+"Amount of instances: " + $amount
+
+# show the relative path of every instance
+# $first = $object | select -First 1
+# $first
+# $first.__RELPATH
+$object | 
+    select DeviceID, __RELPATH
