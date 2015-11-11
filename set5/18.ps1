96e023888fdfc7544e98444cfb3160d41407ab3e
EMerckx <ewoutmerckx.pro@gmail.com>
2015-11-11 21:50:18 +0100

    exercise 18 added

---
+++ set5/18.ps1
@@ -0,0 +1,22 @@
+﻿# Oefening 18
+
+# Toon alle methodes + methodequalifiers voor Win32_Process.
+# De uitvoer afwerken zodat je alle qualifiers ook ziet is niet 
+# zo evident. Gebruik daarvoor foreach + Write-Host.
+
+clear
+
+# get the object of the Win32_Process
+$object = Get-WmiObject -List Win32_Process
+
+# get all the methods of the object
+$methods = $object.Methods
+
+# show the methods and its qualifiers on screen
+$methods | 
+    foreach {
+        $_.Name
+        "`n"
+        $_.Qualifiers
+        "---------------`n"
+     }
